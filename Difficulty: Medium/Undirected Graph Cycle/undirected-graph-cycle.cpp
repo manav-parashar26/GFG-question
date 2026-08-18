aa91cf64ba2124bc0b4d4b858e9a3a1f42765c9a
Manav Parashar <manavparashar26@gmail.com>
2026-08-18 15:36:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,36 +1,26 @@
 class Solution {
-    bool detect(int src, vector<int>& vis, vector<vector<int>>& adj) {
-        vis[src] = 1;
-        queue<pair<int,int>> q;
-        q.push({src, -1});
-        while (!q.empty()) {
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for (auto adjnode : adj[node]) {
+    bool detect(int node,int parent, vector<int>& vis, vector<vector<int>>& adj) {
+        vis[node] = 1;
+        for (auto adjnode : adj[node]) {
                 if (!vis[adjnode]) {
-                    vis[adjnode] = 1;
-                    q.push({adjnode, node});
+                    if(detect(adjnode,node,vis,adj) == true)return true;
                 }
                 else if (parent != adjnode) return true;
             }
-        }
+        
         return false;
     }
-
 public:
     bool isCycle(int V, vector<vector<int>>& edges) {
-        // Build adjacency list from edge pairs
         vector<vector<int>> adj(V);
         for (auto& e : edges) {
             int u = e[0], v = e[1];
             adj[u].push_back(v);
-            adj[v].push_back(u);  // undirected: add both directions
+            adj[v].push_back(u); 
         }
-
         vector<int> vis(V, 0);
         for (int i = 0; i < V; i++) {
-            if (!vis[i] && detect(i, vis, adj)) return true;
+            if (!vis[i] && detect(i,-1, vis, adj) == true) return true;
         }
         return false;
     }
