bed4f0d5c0821c877c45a412da39b8293dc98f61
Manav Parashar <manavparashar26@gmail.com>
2026-09-21 23:48:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        int n = height.size();
+        int prev = 0;
+        int prev2 = 0 ;
+        for(int  i = 1 ; i < n ; i++){
+            int fs = prev + abs(height[i] - height[i-1]);
+            int ss = INT_MAX;
+            if(i > 1) ss = prev2 + abs(height[i] - height[i-2]);
+            int curr = min(fs,ss);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
